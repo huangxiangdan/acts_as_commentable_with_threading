9b4dd536c3e6d173e121f7c4f37cdcb14a575732
huangxiangdan <xd_huang1986@163.com>
2015-08-05 15:11:10 +0800

    add config for model name

--- lib/acts_as_commentable_with_threading.rb
+++ lib/acts_as_commentable_with_threading.rb
@@ -14,9 +14,22 @@ module Acts #:nodoc:
   module CommentableWithThreading #:nodoc:
   extend ActiveSupport::Concern
 
+  class << self
+    attr_reader :model_name
+
+    def model_name= model_name = '::Comment'
+      @model_name = model_name
+      unless @model_name.start_with?('::')
+        @model_name = "::#{model_name}"
+      end
+    end
+
+    model_name = '::Comment'
+  end
+
   module ClassMethods
     def acts_as_commentable
-      has_many :comment_threads, :class_name => "Comment", :as => :commentable
+      has_many :comment_threads, :class_name => Acts::CommentableWithThreading.model_name , :as => :commentable
       before_destroy { |record| record.root_comments.destroy_all }
       include Acts::CommentableWithThreading::LocalInstanceMethods
       extend Acts::CommentableWithThreading::SingletonMethods
@@ -28,7 +41,7 @@ module Acts #:nodoc:
     # Helper method to lookup for comments for a given object.
     # This method is equivalent to obj.comments.
     def find_comments_for(obj)
-      Comment.where(:commentable_id => obj.id, :commentable_type => obj.class.base_class).order('created_at DESC')
+      Acts::CommentableWithThreading.model_name.constantize.where(:commentable_id => obj.id, :commentable_type => obj.class.base_class).order('created_at DESC')
     end
 
     # Helper class method to lookup comments for
@@ -36,7 +49,7 @@ module Acts #:nodoc:
     # This method is NOT equivalent to Comment.find_comments_for_user
     def find_comments_by_user(user)
       commentable = self.base_class.name.to_s
-      Comment.where(:user_id => user.id, :commentable_type => commentable).order('created_at DESC')
+      Acts::CommentableWithThreading.model_name.constantize.where(:user_id => user.id, :commentable_type => commentable).order('created_at DESC')
     end
   end
 
@@ -49,7 +62,7 @@ module Acts #:nodoc:
 
     # Helper method to sort comments by date
     def comments_ordered_by_submitted
-      Comment.where(:commentable_id => id, :commentable_type => self.class.name).order('created_at DESC')
+      Acts::CommentableWithThreading.model_name.constantize.where(:commentable_id => id, :commentable_type => self.class.name).order('created_at DESC')
     end
 
     # Helper method that defaults the submitted time.
